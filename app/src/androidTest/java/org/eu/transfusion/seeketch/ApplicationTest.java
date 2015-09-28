289445571a90c7187e13202fec188486f99afa84
Transfusion <bryan.wyern1@gmail.com>
2015-09-28 08:44:07 +0800

    NavigationView test.

--- app/src/androidTest/java/org/eu/transfusion/seeketch/ApplicationTest.java
+++ app/src/androidTest/java/org/eu/transfusion/seeketch/ApplicationTest.java
@@ -1,4 +1,4 @@
-package org.eu.transfusion.mockup;
+package org.eu.transfusion.seeketch;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
---
+++ app/src/main/java/org/eu/transfusion/seeketch/navDrawerAdapter.java
@@ -0,0 +1,36 @@
+package org.eu.transfusion.seeketch;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by transfusion on 15-9-27.
+ */
+/*
+public class navDrawerAdapter extends RecyclerView.Adapter<navDrawerAdapter.navDrawerViewHolder>{
+
+    public static class navDrawerViewHolder extends RecyclerView.ViewHolder {
+
+        boolean isHeader;
+        TextView itemName;
+        ImageView itemIcon;
+        navDrawerViewHolder(View v, boolean isHeader){
+            super(v);
+            this.isHeader = isHeader;
+            if (isHeader){
+
+            }
+            else {
+                itemName = (TextView) itemView.findViewById(R.id.rowText);
+                itemIcon = (ImageView) itemView.findViewById(R.id.rowIcon);
+            }
+        }
+
+    }
+
+    navDrawerAdapter(String[] navItems, int[] ){
+
+    }
+}*/
